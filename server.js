a7890dfeaabb09038355ad9e0dccd26e7d4b7886
Held Grijo <held.grijo@gmail.com>
2021-03-29 12:27:25 -0400

    Template funcional do chat

--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 const { renderFile } = require('ejs');
 const express = require('express');
 const path = require('path');
-
+const port = 3001;
 const app = express();
 
 // definindo protocolo http
 const server = require('http').createServer(app);
 // definindo protocolo wss para o websocket
 const io = require('socket.io')(server);
+
 // definido diretório dos arquivos públicos
 app.use(express.static(path.join(__dirname, 'public')));
 // definir para o node que as views são html
 app.set('views', path.join(__dirname, 'public'));
 // definir a engine para html
-app.engine('html', require('ejs'), renderFile);
+app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.use('/', (req, res) => {
   res.render('index.html');
 });
 
-server.listen(3001);
+server.listen(port, () => {
+  console.log('Server está escutando a porta *:' + port);
+});
